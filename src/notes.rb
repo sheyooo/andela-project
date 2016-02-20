eba54c32aefaf5fbd6fb903105fa14f2c1308890
Seyi Adekoya <sheyoootech@gmail.com>
2016-02-20 21:19:05 +0100

    Changed indentation to follow ruby style guide

--- src/notes.rb
+++ src/notes.rb
@@ -1,126 +1,126 @@
 module Sheyi
 
-	class NotesApplication
+  class NotesApplication
 
-		attr_accessor :author
-		attr_reader :notes
-
-
-		def initialize(author)
-			author = clean_string(author)
-			if validate(author)
-				@author = author
-				@notes = []
-			else
-				raise Exception.new "No name supplied!"
-			end
-
-			@list_lambda = lambda { |i|
-				puts "\n"
-				puts "Note ID: #{@notes.index(i)}"
-				puts i[:content]+ "\n\n"
-				puts "By Author #{i[:author]} \n\n"
-			}
-		end
-
-		def create(content)
-
-			if validate(content)
-				note = Hash.new
-				note[:author] = @author
-				note[:content] = content
-
-				@notes.push(note)
-				note
-			else
-				puts_error("Your content cant be empty!")
-			end
-		end
-
-		def list()
-			
-			for i in @notes do
-				@list_lambda.call(i)
-			end
-
-			if @notes.length == 0
-				puts "=== No notes yet! Create one"
-			end
-
-			@notes
-		end
-
-		def get(note_id)
-			if @notes[note_id] != nil
-				return @notes[note_id][:content]
-			else
-				#puts "=== I could not find that note try another ID"
-				nil
-			end
-		end
-
-		def search(text)
-			founds = []
-			puts "<<<< Showing results for '\e[#{32}m #{text}\e[0m' >>>>"
-
-			match = false
-			for i in @notes do
-				 
-				if (i[:content] =~ /#{text}/i)
-					@list_lambda.call(i)
-					founds.push(@notes.index(i))
-					match = true
-				end
-				
-				puts "=== No matches found" unless match
-
-			end
-
-			founds
-		end
-
-		def delete(note_id)
-			if @notes[note_id] != nil
-				@notes.delete_at(note_id)
-			end
-		end
-
-		def edit(note_id, content)
-			if validate(content) && (@notes[note_id] != nil)
-				@notes[note_id][:content] = content
-			else
-				#puts_error("You input is invalid")
-				nil
-			end
-		end
-
-		def validate(content)
-			if content.length > 0
-				return true
-			else
-				return false
-			end
-		end
-
-		def clean_string(str)
-			str.strip!
-			#str.gsub!(/[\\\*\/]/, "") #Removing "/", "\" or "*" from string
-			str
-		end
-
-		def puts_error(msg = "=== ***An error occured***")
-
-			puts msg
-		end
+    attr_accessor :author
+    attr_reader :notes
 
 
-	end
+    def initialize(author)
+      author = clean_string(author)
+      if validate(author)
+        @author = author
+        @notes = []
+      else
+        raise Exception.new "No name supplied!"
+      end
+
+      @list_lambda = lambda { |i|
+        puts "\n"
+        puts "Note ID: #{@notes.index(i)}"
+        puts i[:content]+ "\n\n"
+        puts "By Author #{i[:author]} \n\n"
+      }
+    end
+
+    def create(content)
+
+      if validate(content)
+        note = Hash.new
+        note[:author] = @author
+        note[:content] = content
+
+        @notes.push(note)
+        note
+      else
+        puts_error("Your content cant be empty!")
+      end
+    end
+
+    def list
+      
+      for i in @notes do
+        @list_lambda.call(i)
+      end
+
+      if @notes.length == 0
+        puts "=== No notes yet! Create one"
+      end
+
+      @notes
+    end
+
+    def get(note_id)
+      if @notes[note_id] != nil
+        return @notes[note_id][:content]
+      else
+        #puts "=== I could not find that note try another ID"
+        nil
+      end
+    end
+
+    def search(text)
+      founds = []
+      puts "<<<< Showing results for '\e[#{32}m #{text}\e[0m' >>>>"
+
+      match = false
+      for i in @notes do
+         
+        if (i[:content] =~ /#{text}/i)
+          @list_lambda.call(i)
+          founds.push(@notes.index(i))
+          match = true
+        end
+        
+        puts "=== No matches found" unless match
+
+      end
+
+      founds
+    end
+
+    def delete(note_id)
+      if @notes[note_id] != nil
+        @notes.delete_at(note_id)
+      end
+    end
+
+    def edit(note_id, content)
+      if validate(content) && (@notes[note_id] != nil)
+        @notes[note_id][:content] = content
+      else
+        #puts_error("You input is invalid")
+        nil
+      end
+    end
+
+    def validate(content)
+      if content.length > 0
+        return true
+      else
+        return false
+      end
+    end
+
+    def clean_string(str)
+      str.strip!
+      #str.gsub!(/[\\\*\/]/, "") #Removing "/", "\" or "*" from string
+      str
+    end
+
+    def puts_error(msg = "=== ***An error occured***")
+
+      puts msg
+    end
+
+
+  end
 end
 
 =begin
-	app = Sheyi::NotesApplication.new "Sheyi"
-	app.create("h")
-	app.create("ggh")
-	puts app.list
+  app = Sheyi::NotesApplication.new "Sheyi"
+  app.create("h")
+  app.create("ggh")
+  puts app.list
 
 =end
--- src/notes_user.rb
+++ src/notes_user.rb
@@ -3,138 +3,138 @@
 require_relative "notes"
 
 
-	$app = nil
+  $app = nil
 
-	def main
-		puts "<<<<< Hello there, what is your name? >>>>>"
-		name = gets.chomp.to_s
+  def main
+    puts "<<<<< Hello there, what is your name? >>>>>"
+    name = gets.chomp.to_s
 
-		begin
-			$app = Sheyi::NotesApplication.new (name)
-		rescue Exception
-			puts "=== I need a name to work with! ||| \e[#{31}m Error\e[0m"
-			main
-		end
-		puts "=== Welcome, #{$app.author} \n\n"
-
-
-		menu
-	end
-
-	def menu
-		puts "<<< Hello #{$app.author}, what do you want to do? >>>"
-		puts "[1] ====== Create a note"
-		puts "[2] ====== Read a note"
-		puts "[3] ====== Edit a note"
-		puts "[4] ====== Delete a note"
-		puts "[5] ====== Search"
-		puts "[6] ====== List all notes"
-		puts "[7] ====== Change author"
-		puts "[0] ====== Exit"
-		choice = gets.chomp.to_i
-
-		case choice
-		when 1
-			create
-		when 2
-			read
-		when 3
-			edit
-		when 4
-			delete
-		when 5
-			search
-		when 6
-			list
-		when 7
-			change_user
-		when 0
-			exit!()
-		else
-			puts "====== That option doesn't exist. Use a number from 1 to 5 ======\n\n"
-			menu
-		end
-	end
-
-	def create 
-		puts "==== Wrtie a note"
-		note = gets.chomp.to_s
-
-		note = $app.create(note)
-		puts "=== Your note id is #{$app.notes.index(note)}" if note != nil
-		menu
-	end
-
-	def list
-		$app.list
-		menu
-	end
-
-	def read
-		puts "=== What is the id of the note you want to read?"
-		id = gets.chomp.to_i
-		note = $app.get(id)
-
-		if note != nil
-			puts "Note ID: #{id}"
-			puts note
-		else
-			puts "=== I couldn't find that note! ==="
-		end
-		menu
-	end
-
-	def edit
-		puts "=== Which note do you want to edit, put in an ID."
-		id = gets.chomp.to_i
-
-		if $app.get(id)
-			puts "=== Input the new content you want for the note. Make sure you write something"
-			new_content = gets.chomp.to_s
-			res = $app.edit(id, new_content)
-			if res == nil
-				puts "=== Your note wasn't edited"
-			else
-				puts "=== Success"
-			end
-		end
-
-		menu
-	end
-
-	def delete
-		puts "=== Input the ID of the note you want to delete."
-		id = gets.chomp.to_i
-
-		$app.delete(id)
-		puts "=== Deleted"
-		menu
-	end
-
-	def search 
-		puts "=== Input search text."
-		text = gets.chomp.to_s
-
-		$app.search(text)
-
-		menu
-	end
-
-	def change_user
-		puts "<<<< What is your name? >>>>"
-		author = gets.chomp
-
-		if $app.validate(author)
-			$app.author = author
-		else
-			puts "=== Put a valid name"
-			change_user
-		end
-
-		menu
-	end
+    begin
+      $app = Sheyi::NotesApplication.new (name)
+    rescue Exception
+      puts "=== I need a name to work with! ||| \e[#{31}m Error\e[0m"
+      main
+    end
+    puts "=== Welcome, #{$app.author} \n\n"
 
 
+    menu
+  end
 
-	main
+  def menu
+    puts "<<< Hello #{$app.author}, what do you want to do? >>>"
+    puts "[1] ====== Create a note"
+    puts "[2] ====== Read a note"
+    puts "[3] ====== Edit a note"
+    puts "[4] ====== Delete a note"
+    puts "[5] ====== Search"
+    puts "[6] ====== List all notes"
+    puts "[7] ====== Change author"
+    puts "[0] ====== Exit"
+    choice = gets.chomp.to_i
+
+    case choice
+    when 1
+      create
+    when 2
+      read
+    when 3
+      edit
+    when 4
+      delete
+    when 5
+      search
+    when 6
+      list
+    when 7
+      change_user
+    when 0
+      exit!()
+    else
+      puts "====== That option doesn't exist. Use a number from 1 to 5 ======\n\n"
+      menu
+    end
+  end
+
+  def create 
+    puts "==== Wrtie a note"
+    note = gets.chomp.to_s
+
+    note = $app.create(note)
+    puts "=== Your note id is #{$app.notes.index(note)}" if note != nil
+    menu
+  end
+
+  def list
+    $app.list
+    menu
+  end
+
+  def read
+    puts "=== What is the id of the note you want to read?"
+    id = gets.chomp.to_i
+    note = $app.get(id)
+
+    if note != nil
+      puts "Note ID: #{id}"
+      puts note
+    else
+      puts "=== I couldn't find that note! ==="
+    end
+    menu
+  end
+
+  def edit
+    puts "=== Which note do you want to edit, put in an ID."
+    id = gets.chomp.to_i
+
+    if $app.get(id)
+      puts "=== Input the new content you want for the note. Make sure you write something"
+      new_content = gets.chomp.to_s
+      res = $app.edit(id, new_content)
+      if res == nil
+        puts "=== Your note wasn't edited"
+      else
+        puts "=== Success"
+      end
+    end
+
+    menu
+  end
+
+  def delete
+    puts "=== Input the ID of the note you want to delete."
+    id = gets.chomp.to_i
+
+    $app.delete(id)
+    puts "=== Deleted"
+    menu
+  end
+
+  def search 
+    puts "=== Input search text."
+    text = gets.chomp.to_s
+
+    $app.search(text)
+
+    menu
+  end
+
+  def change_user
+    puts "<<<< What is your name? >>>>"
+    author = gets.chomp
+
+    if $app.validate(author)
+      $app.author = author
+    else
+      puts "=== Put a valid name"
+      change_user
+    end
+
+    menu
+  end
+
+
+
+  main
 
